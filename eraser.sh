0b81e57f6f5920c23ff7b6679a42e6143b333c99
vikalejo <victor.alejo@wuaki.tv>
2014-07-09 22:10:35 +0200

    Edit style format

--- eraser.sh
+++ eraser.sh
@@ -10,158 +10,206 @@
 	echo "*******************************************************************"
 	
 	function eraser() {
-	clear	
-	echo "Please, insert the field you want to delete of the database."
-	read FIELD
-	FIELD=$(echo $FIELD | tr "[:lower:]" "[:upper:]")
-	
-	echo "Please, insert the name of the table (example 'address0') of the field you want to delete."
-	read TABLE0
-	TABLE0=$(echo $TABLE0 | tr "[:lower:]" "[:upper:]")		
-	TABLE=$(expr "$TABLE0" : '\(.*\).')
-	
-	echo "Is the field $FIELD a string(s) or a selection value(v)?. Please write 's' or 'v' "
-	read TYPE
-			
-		case $TYPE in
-			[sS] )
-				delete_string
-			;;
-			[vV] )
-				delete_svalue
-			;;
-			*)
-				wrong
-			;;
-		esac
-				
-		
+    clear	
+    echo "Please, insert the field you want to delete of the database."
+    read FIELD
+    FIELD=$(echo $FIELD | tr "[:lower:]" "[:upper:]")
+    
+    echo "Please, insert the name of the table (example 'address0') of the field you want to delete."
+    read TABLE0
+    TABLE0=$(echo $TABLE0 | tr "[:lower:]" "[:upper:]")		
+    TABLE=$(expr "$TABLE0" : '\(.*\).')
+    
+    echo "Is the field $FIELD a string(s) or a selection value(v)?. Please write 's' or 'v' "
+    read TYPE
+        
+      case $TYPE in
+        [sS] )
+          delete_string
+        ;;
+        [vV] )
+          delete_svalue
+        ;;
+        *)
+          wrong
+        ;;
+      esac
 	} 	
 		
 	function delete_string(){
 	
-	echo "You are going to delete the string '$FIELD' of the table '$TABLE0', are you sure?"
-	echo "yes(y) or cancel(any key)"
-	read Check
-				
-	if [ $Check == "y" ] || [ $Check == "Y" ]	# delete it
-	cont=0
-	then
-		for t in $DBS ; do
-			if  [ $t == "development" -o $t == "pspesslingen" -o $t == "wohlfahrtswerk" -o $t == "test1" -o $t == "test2" -o $t == "test3" -o $t == "test4" -o $t == "test5" -o $t == "test6" -o $t == "test7" -o $t == "test8" -o $t == "test9" -o $t == "test10" ]
-			then
-				#flag=0
-				
-				CheckData # Check if the field exits in the Columnseim table of the database, and it is a string type.
-					
-				if [ "${row[5]}" != "STRING" ] || [ "${row[3]}" == "" ] || [ "${row[4]}" == "" ] || [ "${row[5]}" == "" ] || [ "${row[4]}" != "$FIELD" ] || [ "${row[3]}" != "$TABLE0" ]
-				then
-				 
-					echo "** Database '$t'. The data introduced is not correct or not exists in this database."
-					echo -e "\n"
-			 		((cont++))
-					
-				else # The field exits and it is a string
-					echo "Database '$t'. Field exists in the database...Ok."
-					echo "Deleting the field..."
-					
-					D_T=`date +%Y%m%d`   
-					mysql -u root -p$PS -D $t -e "select * FROM SYSCOLUMNSEIM WHERE TABLENAME LIKE '$TABLE0' AND COLNAME LIKE '$FIELD' AND DICTIONARYKEY LIKE '$TABLE.$FIELD';" ls -al 2>&1 | tee -a eraser.$D_T.log
-					#mysql -u root -p$PS -D $t -e "DELETE FROM SYSCOLUMNSEIM WHERE TABLENAME LIKE '$TABLE0' AND COLNAME LIKE '$FIELD' AND DICTIONARYKEY LIKE '$TABLE.$FIELD';" 2>&1 | tee -a eraser.$D_T.log
-					#mysql -u root -p$PS -D $t -e "ALTER TABLE $TABLE0 DROP $FIELD;" 2>&1 | tee -a eraser.$D_T.log
-					
-					CheckData 
-						
-					if [ "${row[4]}" == "" ] # The column was deleted
-					then
-						echo "** Database '$t' *********************** deleting field....done"
-					else
-						echo "** There were errors in the execution. Please, check the information of the database "
-												
-					fi	
-										
-				fi
-			else
-				echo "Database '$t'. Does not have changes"
-			fi
-		done
-		terminator
-		again
-	else
-		again
-	fi
-			
+    echo "You are going to delete the string '$FIELD' of the table '$TABLE0', are you sure?"
+    echo "yes(y) or cancel(any key)"
+    read Check
+          
+    if [ $Check == "y" ] || [ $Check == "Y" ]	# delete it
+      cont=0
+    then
+      for t in $DBS ; do
+        if  [ 
+          $t == "development" -o 
+          $t == "pspesslingen" -o 
+          $t == "wohlfahrtswerk" -o 
+          $t == "test1" -o 
+          $t == "test2" -o 
+          $t == "test3" -o 
+          $t == "test4" -o 
+          $t == "test5" -o 
+          $t == "test6" -o 
+          $t == "test7" -o 
+          $t == "test8" -o 
+          $t == "test9" -o 
+          $t == "test10" 
+          ]
+        then
+          #flag=0
+          
+          CheckData # Check if the field exits in the Columnseim table of the database, and it is a string type.
+            
+          if [ "${row[5]}" != "STRING" ] || 
+            [ "${row[3]}" == "" ] || 
+            [ "${row[4]}" == "" ] || 
+            [ "${row[5]}" == "" ] || 
+            [ "${row[4]}" != "$FIELD" ] || 
+            [ "${row[3]}" != "$TABLE0" ]
+          then
+           
+            echo "** Database '$t'. The data introduced is not correct or not exists in this database."
+            echo -e "\n"
+            ((cont++))
+            
+          else # The field exits and it is a string
+            echo "Database '$t'. Field exists in the database...Ok."
+            echo "Deleting the field..."
+            
+            D_T=`date +%Y%m%d`   
+
+            mysql -u root -p$PS -D $t -e 
+              "select * FROM SYSCOLUMNSEIM WHERE TABLENAME LIKE '$TABLE0' 
+              AND COLNAME LIKE '$FIELD' 
+              AND DICTIONARYKEY LIKE '$TABLE.$FIELD';" ls -al 2>&1 | tee -a eraser.$D_T.log
+
+            CheckData 
+              
+            if [ "${row[4]}" == "" ] # The column was deleted
+            then
+              echo "** Database '$t' *********************** deleting field....done"
+            else
+              echo "** There were errors in the execution. Please, check the information of the database "
+            fi	
+          fi
+
+        else
+          echo "Database '$t'. Does not have changes"
+        fi
+      done
+
+      terminator
+      again
+      
+    else
+      again
+    fi
 	}
 		
 	function delete_svalue() {
-		
-	echo "You are going to delete the selection value '$FIELD' of the table '$TABLE0', are you sure?"
-	echo "yes(y) or cancel(any key)"
-	read Check		
-	
-	if [ $Check == "y" ] || [ $Check == "Y" ] # delete the selection value
-	then
-		cont=0
-		for t in $DBS ; do
-			if [ $t == "development" -o $t == "pspesslingen" -o $t == "wohlfahrtswerk" -o $t == "test1" -o $t == "test2" -o $t == "test3" -o $t == "test4" -o $t == "test5"	-o $t == "test6" -o $t == "test7" -o $t == "test8" -o $t == "test9" -o $t == "test10" ]
-			then	
-				
-				CheckData # Check if the field exits in the Columnseim table of the database, and it is a string type.
-						
-				if [ "${row[5]}" != "SELECTIONVALUELIST" ] || [ "${row[3]}" == "" ] || [ "${row[4]}" == "" ] || [ "${row[5]}" == "" ] || [ "${row[4]}" != "$FIELD" ] || [ "${row[3]}" != "$TABLE0" ]
-				then
-					echo "** Database '$t'. The data introduced is not correct or not exists in this database."
-					echo -e "\n"
-			 		((cont++))
-					
-				else	
-					echo "Database '$t'. Field exists in the database '$t'...Ok."
-					echo "Deleting the field..."
-					
-					D_T=`date +%Y%m%d`
-					mysql -u root -p$PS -D $t -e "DELETE FROM SELECTIONVALUESRELATION WHERE COLNAME LIKE '$FIELD' AND TYPE LIKE '$TABLE';" 1>&2 | tee -a eraser.$D_T.log
-					mysql -u root -p$PS -D $t -e "DELETE FROM SELECTIONVALUES WHERE COLNAME LIKE '$FIELD' AND DICTIONARYKEY LIKE '$TABLE.$FIELD%' AND TYPE LIKE '$TABLE';" 2>&1 | tee -a eraser.$D_T.log
-					mysql -u root -p$PS -D $t -e "DELETE FROM SYSCOLUMNSEIM WHERE TABLENAME LIKE '$TABLE0' AND COLNAME LIKE '$FIELD';" 2>&1 | tee -a eraser.$D_T.log
-					mysql -u root -p$PS -D $t -e "ALTER TABLE $TABLE0 DROP $FIELD;" 1>&2 | tee -a eraser.$D_T.log
-															
-					CheckData 
-					
-					if [ "${row[4]}" == "" ] # The column was deleted
-					then
-						echo -e "** Database '$t' *********************** deleting field....done \n"
-					else
-						echo "** There were errors in the execution. Please, check the information of the database"
-					fi
-				
-				fi			
-			else
-				echo "Database '$t'. Does not have changes"
-			fi 
-		done
-		terminator
-		again
-	else
-		again
-	fi		
-		
+
+    echo "You are going to delete the selection value '$FIELD' of the table '$TABLE0', are you sure?"
+    echo "yes(y) or cancel(any key)"
+    read Check		
+    
+    if [ $Check == "y" ] || [ $Check == "Y" ] # delete the selection value
+    then
+      cont=0
+
+      for t in $DBS ; do
+        if [ 
+          $t == "development" -o
+          $t == "pspesslingen" -o 
+          $t == "wohlfahrtswerk" -o 
+          $t == "test1" -o 
+          $t == "test2" -o 
+          $t == "test3" -o 
+          $t == "test4" -o 
+          $t == "test5"	-o 
+          $t == "test6" -o 
+          $t == "test7" -o 
+          $t == "test8" -o 
+          $t == "test9" -o 
+          $t == "test10" 
+          ]
+        then	
+          
+          CheckData # Check if the field exits in the Columnseim table of the database, and it is a string type.
+              
+          if [ "${row[5]}" != "SELECTIONVALUELIST" ] || 
+            [ "${row[3]}" == "" ] || 
+            [ "${row[4]}" == "" ] || 
+            [ "${row[5]}" == "" ] || 
+            [ "${row[4]}" != "$FIELD" ] || 
+            [ "${row[3]}" != "$TABLE0" ]
+          then
+            echo "** Database '$t'. The data introduced is not correct or not exists in this database."
+            echo -e "\n"
+            ((cont++))
+            
+          else	
+            echo "Database '$t'. Field exists in the database '$t'...Ok."
+            echo "Deleting the field..."
+            
+            D_T=`date +%Y%m%d`
+            mysql -u root -p$PS -D $t -e 
+              "DELETE FROM SELECTIONVALUESRELATION WHERE COLNAME LIKE '$FIELD' 
+              AND TYPE LIKE '$TABLE';" 1>&2 | tee -a eraser.$D_T.log
+
+            mysql -u root -p$PS -D $t -e 
+              "DELETE FROM SELECTIONVALUES WHERE COLNAME LIKE '$FIELD' AND 
+              DICTIONARYKEY LIKE '$TABLE.$FIELD%' AND 
+              TYPE LIKE '$TABLE';" 2>&1 | tee -a eraser.$D_T.log
+
+            mysql -u root -p$PS -D $t -e 
+              "DELETE FROM SYSCOLUMNSEIM WHERE TABLENAME LIKE '$TABLE0' AND 
+              COLNAME LIKE '$FIELD';" 2>&1 | tee -a eraser.$D_T.log
+
+            mysql -u root -p$PS -D $t -e "ALTER TABLE $TABLE0 DROP $FIELD;" 1>&2 | tee -a eraser.$D_T.log
+                                
+            CheckData 
+            
+            if [ "${row[4]}" == "" ] # The column was deleted
+            then
+              echo -e "** Database '$t' *********************** deleting field....done \n"
+            else
+              echo "** There were errors in the execution. Please, check the information of the database"
+            fi
+          
+          fi			
+        else
+          echo "Database '$t'. Does not have changes"
+        fi 
+      done
+
+      terminator
+      again
+    else
+      again
+    fi		
 	}
 		
 	function again() {
-	echo "Press any key to continue deleting fields, or (e) to exit"
-	read continue
+	  echo "Press any key to continue deleting fields, or (e) to exit"
+	  read continue
 		
 		if [ $continue == "e" ]
 		then
 			exit 1
 		else
 			eraser
-		
 		fi
 	} 
 	
 	function wrong(){
-	echo "Please, select the correct option. Press any key to continue or (e) to exit."
-	read continue
+	  echo "Please, select the correct option. Press any key to continue or (e) to exit."
+	  read continue
 		if [ $continue == "e" ]
 		then
 			exit 2
@@ -173,19 +221,19 @@
 	}
 	
 	function CheckData(){
-	#Check if the field is a string value
-	
-	check_string=$(mysql -u root -p$PS -D $t -e "SELECT TABLENAME,COLNAME,COLTYPE FROM SYSCOLUMNSEIM WHERE COLNAME LIKE '$FIELD' AND TABLENAME LIKE '$TABLE0';")
-	row=( $( for i in $check_string ; do echo $i ; done ) )					
+    #Check if the field is a string value
+    
+    check_string=$(mysql -u root -p$PS -D $t -e "SELECT TABLENAME,COLNAME,COLTYPE FROM SYSCOLUMNSEIM WHERE COLNAME LIKE '$FIELD' AND TABLENAME LIKE '$TABLE0';")
+    row=( $( for i in $check_string ; do echo $i ; done ) )					
 	}
 	
 	function terminator(){
 	
-	if [ $cont -eq 13 ]; then 
-	echo "Nothing was deleted in any database."
-	else
+    if [ $cont -eq 13 ]; then 
+	    echo "Nothing was deleted in any database."
+	  else
 	
-		echo "
+		  echo "
 		              _______
                      <((((((\\\\\\
                      /      . }\\
@@ -203,14 +251,13 @@
           \\  \\      \\        /		  	
 	"
 	
-	echo -e "** You shoud update the database and restart Tomcat Server.\n"
-	fi
+	    echo -e "** You shoud update the database and restart Tomcat Server.\n"
+	  fi
 	}
 	
 		
 if [ $(whoami) != "root" ] ; then
-	
-	echo "You should be root to run this script."
+  echo "You should be root to run this script."
 	else
 		while [ ls $DBS 2>/dev/null != 0 ]; do
 			echo "Please, enter the password to connect with the databases"
@@ -220,5 +267,4 @@ if [ $(whoami) != "root" ] ; then
 		
 	clear
 	eraser
-			
 fi	
--- eraserSelectDDBB.sh
+++ eraserSelectDDBB.sh
@@ -7,22 +7,30 @@
   #				  - New functions: *showSelectDataBases. Check and show the ddbb in the machine and save the selection of the user.
   #								   *saveDB.Link with the showSelectDataBases function and control the repetion of the ddbb selected.  		  
   
-	function eraser(){
+function eraser(){
 
 	if [ "$check" == "y" ] || [ "$check" == "Y" ]	# delete it
 	then
-		if [ $TYPE == "STRING" -o $TYPE == "INT" -o $TYPE == "BOOLEAN" -o $TYPE == "DATETIME" ]
+		if 
+      [ $TYPE == "STRING" -o 
+        $TYPE == "INT" -o 
+        $TYPE == "BOOLEAN" -o 
+        $TYPE == "DATETIME" 
+      ]
 		then
 			deleteSimpleField
 		else
+
 			if [ $TYPE=="SELECTIONVALUELIST" ]
 			then
 				deleteSelectionValue
 			fi
 		fi
+    
 	else
 		echo "Canceled. Press any key to continue or (e) to exit."
 		read continue
+
 		if [ $continue == "e" ]
 		then
 			exit 1
@@ -36,123 +44,141 @@
 	} 	
 
 	function deleteSimpleField(){
-	
-	cont=0
-	contFor=0
-	D_T=`date +%Y%m%d`  
-	LOG=eraser.$D_T.log
+    cont=0
+    contFor=0
+    D_T=`date +%Y%m%d`  
+    LOG=eraser.$D_T.log
 
-	while [ $c -gt 0 -o $c -eq 0 ]; do
-		t=${selectedDB[c]}
-		mysql -u root -p$PS -D $t -e "DELETE FROM SYSCOLUMNSEIM WHERE TABLENAME LIKE '$TABLE0' AND COLNAME LIKE '$FIELD' AND DICTIONARYKEY LIKE '$TABLE.$FIELD';" | tee -a $LOG 
-		mysql -u root -p$PS -D $t -e "ALTER TABLE $TABLE0 DROP $FIELD;" | tee -a $LOG
-			
-	
-		#CheckData # Check if the data was deleted
-		delete=1	
-		checkData
-			if [ "${row[4]}" == "" ] # The column was deleted
-			then
-				echo "**	Database '$t' *********************** deleting field....done"
-			else
-				echo "**	Database '$t'... There were errors in the execution. Please, check the data of the database "
-				((cont++))						
-			fi	
-			((contFor++))
+    while [ $c -gt 0 -o $c -eq 0 ]; do
+      t=${selectedDB[c]}
+      mysql -u root -p$PS -D $t -e 
+        "DELETE FROM SYSCOLUMNSEIM WHERE TABLENAME LIKE '$TABLE0' AND 
+        COLNAME LIKE '$FIELD' AND 
+        DICTIONARYKEY LIKE '$TABLE.$FIELD';" | tee -a $LOG 
 
-		((c--))
-	done
-	terminator
-	again	
-	
+      mysql -u root -p$PS -D $t -e "ALTER TABLE $TABLE0 DROP $FIELD;" | tee -a $LOG
+    
+      #CheckData # Check if the data was deleted
+      delete=1	
+      checkData
+        if [ "${row[4]}" == "" ] # The column was deleted
+        then
+          echo "**	Database '$t' *********************** deleting field....done"
+        else
+          echo "**	Database '$t'... There were errors in the execution. Please, check the data of the database "
+          ((cont++))						
+        fi	
+        ((contFor++))
+
+      ((c--))
+    done
+
+    terminator
+    again	
 	}
 		
 	function deleteSelectionValue(){
-	
-	cont=0
-	contFor=0	
-	D_T=`date +%Y%m%d`  
-	LOG=eraser.$D_T.log
-	
-	while [ $c -gt 0 -o $c -eq 0 ]; do
-		t=${selectedDB[c]}				
-		mysql -u root -p$PS -D $t -e "DELETE FROM SELECTIONVALUESRELATION WHERE COLNAME LIKE '$FIELD' AND TYPE LIKE '$TABLE';"  | tee -a $LOG
-		mysql -u root -p$PS -D $t -e "DELETE FROM SELECTIONVALUES WHERE COLNAME LIKE '$FIELD' AND DICTIONARYKEY LIKE '$TABLE.$FIELD%' AND TYPE LIKE '$TABLE';" | tee -a $LOG
-		mysql -u root -p$PS -D $t -e "DELETE FROM SYSCOLUMNSEIM WHERE TABLENAME LIKE '$TABLE0' AND COLNAME LIKE '$FIELD';" | tee -a $LOG
-		mysql -u root -p$PS -D $t -e "ALTER TABLE $TABLE0 DROP $FIELD;" | tee -a $LOG
-	
-		#CheckData. Check if the data was deleted after the sql scripts were executed.
-		delete=1	
-		checkData
-		if [ "${row[4]}" == "" ] # The column was deleted
-		then
-			echo "**	Database '$t' *********************** deleting field....done"
-		else
-			echo "**	Database '$t'... There were errors in the execution.The name of the table has to be like 'address0'. Please, check the data introduced "
-			((cont++))						
-		fi	
-		((contFor++))
+    cont=0
+    contFor=0	
+    D_T=`date +%Y%m%d`  
+    LOG=eraser.$D_T.log
+    
+    while [ $c -gt 0 -o $c -eq 0 ]; do
+      t=${selectedDB[c]}				
+      mysql -u root -p$PS -D $t -e 
+        "DELETE FROM SELECTIONVALUESRELATION WHERE COLNAME LIKE '$FIELD' AND 
+      TYPE LIKE '$TABLE';"  | tee -a $LOG
 
-		((c--))
-	done
-	terminator
-	again											
-	
+      mysql -u root -p$PS -D $t -e 
+        "DELETE FROM SELECTIONVALUES WHERE COLNAME LIKE '$FIELD' AND 
+        DICTIONARYKEY LIKE '$TABLE.$FIELD%' AND 
+        TYPE LIKE '$TABLE';" | tee -a $LOG
+        
+      mysql -u root -p$PS -D $t -e 
+        "DELETE FROM SYSCOLUMNSEIM WHERE TABLENAME LIKE '$TABLE0' AND 
+        COLNAME LIKE '$FIELD';" | tee -a $LOG
+
+      mysql -u root -p$PS -D $t -e 
+        "ALTER TABLE $TABLE0 DROP $FIELD;" | tee -a $LOG
+    
+      #CheckData. Check if the data was deleted after the sql scripts were executed.
+      delete=1	
+      checkData
+
+      if [ "${row[4]}" == "" ] # The column was deleted
+      then
+        echo "**	Database '$t' *********************** deleting field....done"
+      else
+        echo "**	Database '$t'... There were errors in the execution.The name of the table has to be like 'address0'. Please, check the data introduced "
+        ((cont++))						
+      fi	
+      ((contFor++))
+
+      ((c--))
+    done
+
+    terminator
+    again											
 	}
 				
 	function checkData(){
-	
-	if [ $delete -eq 0 ]
-	then
-	
-		echo "The selected databases are: "
-		echo "-> ${selectedDB[*]} "			
-		echo "Please, insert the field you want to delete of the database."
-		read FIELD
-		FIELD=$(echo $FIELD | tr "[:lower:]" "[:upper:]")
-		echo "Please, insert the name of the table (example 'address0') of the field you want to delete."
-		read TABLE0
-		TABLE0=$(echo $TABLE0 | tr "[:lower:]" "[:upper:]")		
-		TABLE=$(expr "$TABLE0" : '\(.*\).')
-		echo -e "\n"
-		
-		#Check the field in one of the database, in this case 'the first of the array'
-		
-		database=${selectedDB[0]}  # Take the first database to check the data.
-		checkType=$(mysql -u root -p$PS -D $database -e "SELECT TABLENAME,COLNAME,COLTYPE FROM SYSCOLUMNSEIM WHERE COLNAME LIKE '$FIELD' AND TABLENAME LIKE '$TABLE0';")
-		row=( $( for i in $checkType ; do echo $i ; done ) )
-		
-		# Check if the data exits
-			if  [ "${row[5]}" == "" ] || [ "${row[3]}" == "" ] || [ "${row[4]}" == "" ] || [ "${row[5]}" == "" ] || [ "${row[4]}" != "$FIELD" ] || [ "${row[3]}" != "$TABLE0" ] # Don´t exists
-			then
-				echo "** The data introduced not exists in the databases."
-				echo "	Nothing was deleted in any database."
-				echo -e "\n"
-				again
-			else
-				TYPE=${row[5]} 
-				
-				echo "You are going to delete the $TYPE field '$FIELD', of the table '$TABLE0'"
-				echo "are you sure?, yes(y) or cancel(any key)"
-				read check
-				
-				# Recuperate the value of the number of elements			
-				c=$elements
-				eraser
-			fi
-				
-	else
-		# delete=1. The scripts have been executed and check if the data was deleted.
-		database=${selectedDB[c]}  # Value in that moment
-		checkType=$(mysql -u root -p$PS -D $database -e "SELECT TABLENAME,COLNAME,COLTYPE FROM SYSCOLUMNSEIM WHERE COLNAME LIKE '$FIELD' AND TABLENAME LIKE '$TABLE0';")
-		row=( $( for i in $checkType ; do echo $i ; done ) )	
+    if [ $delete -eq 0 ]
+    then
+      echo "The selected databases are: "
+      echo "-> ${selectedDB[*]} "			
+      echo "Please, insert the field you want to delete of the database."
+      read FIELD
+      FIELD=$(echo $FIELD | tr "[:lower:]" "[:upper:]")
+      echo "Please, insert the name of the table (example 'address0') of the field you want to delete."
+      read TABLE0
+      TABLE0=$(echo $TABLE0 | tr "[:lower:]" "[:upper:]")		
+      TABLE=$(expr "$TABLE0" : '\(.*\).')
+      echo -e "\n"
+      
+      #Check the field in one of the database, in this case 'the first of the array'
+      
+      database=${selectedDB[0]}  # Take the first database to check the data.
+      checkType=$(mysql -u root -p$PS -D $database -e "SELECT TABLENAME,COLNAME,COLTYPE FROM SYSCOLUMNSEIM WHERE COLNAME LIKE '$FIELD' AND TABLENAME LIKE '$TABLE0';")
+      row=( $( for i in $checkType ; do echo $i ; done ) )
+      
+      # Check if the data exits
+      if  [ "${row[5]}" == "" ] || 
+        [ "${row[3]}" == "" ] || 
+        [ "${row[4]}" == "" ] || 
+        [ "${row[5]}" == "" ] || 
+        [ "${row[4]}" != "$FIELD" ] || 
+        [ "${row[3]}" != "$TABLE0" ] # Don´t exists
+        
+      then
+        echo "** The data introduced not exists in the databases."
+        echo "	Nothing was deleted in any database."
+        echo -e "\n"
+        
+        again
+      else
+        TYPE=${row[5]} 
+        
+        echo "You are going to delete the $TYPE field '$FIELD', of the table '$TABLE0'"
+        echo "are you sure?, yes(y) or cancel(any key)"
+        read check
+        
+        # Recuperate the value of the number of elements			
+        c=$elements
+        eraser
+      fi
+    else
+      # delete=1. The scripts have been executed and check if the data was deleted.
 
-	fi
-	delete=0
+      database=${selectedDB[c]}  # Value in that moment
+      checkType=$(mysql -u root -p$PS -D $database -e 
+        "SELECT TABLENAME,COLNAME,COLTYPE FROM SYSCOLUMNSEIM WHERE COLNAME LIKE '$FIELD' AND 
+        TABLENAME LIKE '$TABLE0';")
+      row=( $( for i in $checkType ; do echo $i ; done ) )	
+    fi
+    delete=0
 	}
 
 	function saveDB(){
-		
 		if [ $anotherTime -eq 1 ]
 		then
 			echo "Press any key to continuing with the same databases "
@@ -169,83 +195,78 @@
 		else
 			showSelectDataBases
 		fi
-			
 	}
 		
 	function showSelectDataBases(){
+	  # * Show the databases
 
-	# * Show the databases
+    echo " ** Those are the databases in this machine: "
+    echo -e "\n"
+    a=0
+    for ddbb in $DBS 
+      do	
+        machineDB[a]=$ddbb	
+        echo "[$a]-${machineDB[a]}"
+        ((a++))
+    done
 
-	echo " ** Those are the databases in this machine: "
-	echo -e "\n"
-	a=0
-	for ddbb in $DBS 
-		do	
-			machineDB[a]=$ddbb	
-			echo "[$a]-${machineDB[a]}"
-			((a++))
-		
-	done
-	echo -e "\n"
-	# rest the last iteration
-	((a--))
-	
-	# * Choose what databases
-	
-	echo "Please, select between spaces the number of the databases you want to use. Example'(0 2 5)'"
-	DDBB=""
-	read DDBB
+    echo -e "\n"
+    # rest the last iteration
+    ((a--))
+    
+    # * Choose what databases
+    
+    echo "Please, select between spaces the number of the databases you want to use. Example'(0 2 5)'"
+    DDBB=""
+    read DDBB
 
-	if [ -z "$DDBB" ];then #echo "Select at less one database"
-		clear
-		showSelectDataBases
-	else
-	
-		c=0
-		# Empty the whole array
-		unset selectedDB[@]
-		for selectedNumber in $DDBB
-		do
-			if [ $selectedNumber -lt 0 ] || [ $selectedNumber -gt $a ] || [[ $selectedNumber != [0-9]* ]] #! [[ $selectedNumber =~ ^[0-9]+$ ]]
-			#|| [[ $selectedNumber = *[[:digit:]]* ]]
-			then
-				clear
-				echo "Please, insert a correct value"
-				showSelectDataBases
-			else
-			
-				selectedDB[c]=${machineDB[selectedNumber]}	
-				((c++))
+    if [ -z "$DDBB" ];then #echo "Select at less one database"
+      clear
+      showSelectDataBases
+    else
+      c=0
+      
+      # Empty the whole array
+      unset selectedDB[@]
+      for selectedNumber in $DDBB
+       do
+        if [ $selectedNumber -lt 0 ] || 
+          [ $selectedNumber -gt $a ] || 
+          [[ $selectedNumber != [0-9]* ]] #! [[ $selectedNumber =~ ^[0-9]+$ ]]
+                                          #|| [[ $selectedNumber = *[[:digit:]]* ]]
+        then
+          clear
+          echo "Please, insert a correct value"
+          showSelectDataBases
+        else
+          selectedDB[c]=${machineDB[selectedNumber]}	
+          ((c++))
+        fi
+      done
 
-			fi
-		done
-		#Rest the last iteration
-		((c--))
-		# Save the value of 'c' (number of elements)
-		elements=$c
-		checkData
-	fi
-	
+      #Rest the last iteration
+      ((c--))
+      # Save the value of 'c' (number of elements)
+      elements=$c
+      checkData
+    fi
 	}
 	
 	function again(){
-	echo "Press any key to continue deleting fields, or (e) to exit"
-	#initialization go
-	#go="go"
-	#anotherTime=0
-	read go
-		
-		if [ $go == "e" ]
-		then
-			exit 1
-		else
-			clear
-			anotherTime=1			
-			saveDB
-			#checkData
-			#eraser
-		
-		fi
+    echo "Press any key to continue deleting fields, or (e) to exit"
+    #initialization go
+    #go="go"
+    #anotherTime=0
+    read go
+    
+    if [ $go == "e" ]
+    then
+      exit 1
+    else
+      clear
+      anotherTime=1			
+      saveDB
+    fi
 	} 
 	
 	function terminator(){
@@ -285,22 +306,19 @@
 	echo  "	** Delete fields of the database **"
 
 	echo "******************************************************************"
-	
-			if [ $(whoami) != "root" ] ; then
+	  if [ $(whoami) != "root" ] ; then
+		  echo "You should be root to run this script."
+		else
+			while [ ls $DBS 2>/dev/null != 0 ]; do
+				echo "Please, enter the password to connect with the databases"
+				read -s PS
+				DBS=`mysql -u root -p$PS -e "show databases"`
+			done
+      
+		echo "Password OK."
+		echo -e "\n"
 			
-			echo "You should be root to run this script."
-			else
-				while [ ls $DBS 2>/dev/null != 0 ]; do
-					echo "Please, enter the password to connect with the databases"
-					read -s PS
-					DBS=`mysql -u root -p$PS -e "show databases"`
-					
-				done
-			echo "Password OK."
-			echo -e "\n"
-				
-		fi
-	anotherTime=0
-	delete=0
-	saveDB
-	
+	fi
+	  anotherTime=0
+	  delete=0
+	  saveDB
